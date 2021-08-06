874f96922eb138e6de3ef90e3199be03835cbb56
dryamvlad <dryam.vlad@gmail.com>
2021-08-07 00:48:09 +0300

    3d logo

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
   <div class="section space-bottom-large wf-section">
     <div class="container">
       <div class="w-layout-grid hero-2-grid-2"><img src="images/bbb_1gen_v01.png" width="541" sizes="(max-width: 479px) 95vw, (max-width: 767px) 90vw, 541px" srcset="images/bbb_1gen_v01.png 500w, images/bbb_1gen_v01.png 800w, images/bbb_1gen_v01.png 1080w, images/bbb_1gen_v01.png 1200w" alt="" class="rounded-image">
-        <div class="hero-content-2"><img src="images/logo_v02.logo.png" loading="lazy" width="286" sizes="(max-width: 479px) 92vw, 286px" srcset="images/logo_v02.logo.png 500w, images/logo_v02.logo.png 800w, images/logo_v02.logo.png 1000w" alt="">
+        <div class="hero-content-2"><img src="images/logo_v02.logo.png" class="logo" loading="lazy" width="286" sizes="(max-width: 479px) 92vw, 286px" srcset="images/logo_v02.logo.png 500w, images/logo_v02.logo.png 800w, images/logo_v02.logo.png 1000w" alt="">
           <div class="battle-title">
             <h1 class="display-heading-7 battle-title-heading">Own a Bot<br>Battle other Bots<br>Earn Eth</h1>
             <h5 class="panel-subheading text-primary-4">In Battle for Ether</h5>
