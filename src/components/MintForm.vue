ebaaf0d16db836f85e219c37642143fd25d85970
Vlad <dryam.vlad@gmail.com>
2021-12-15 22:29:40 +0530

    webill

--- src/components/MintForm.vue
+++ src/components/MintForm.vue
@@ -19,7 +19,7 @@
             </div>
           </div>
         </div>
-        <div class="center-content-3" v-show="showLoading === false">
+        <div class="center-content-3">
           <div class="text-lead-2 space-bottom-small">
             <br/>
             <span class="text-primary-4"><a href="https://checkout.webill.io/nft/ace4c308-e20d-4a70-82eb-91db2fea7a89">Mint via WeBill</a></span>
